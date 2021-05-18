a2a654beb8681dd136b4d10e546453706fc2a4ec
gksruf2848 <4455qqq@naver.com>
2021-05-18 17:33:39 +0900

    ActionBar 수정

--- app/src/main/java/com/cookandroid/ramenapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/ramenapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.ramenapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //타이틀바 텍스트 (공백)
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("");
+
+        //타이틀바 이미지 삽입
+        getSupportActionBar().setIcon(R.drawable.logo_title);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
 
