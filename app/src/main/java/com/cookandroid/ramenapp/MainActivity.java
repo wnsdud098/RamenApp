d282f6638062d96a8686907896b6185452484eff
gksruf2848 <4455qqq@naver.com>
2021-05-20 15:58:33 +0900

    main toolbar 추가

--- app/src/main/java/com/cookandroid/ramenapp/MainActivity.java
+++ app/src/main/java/com/cookandroid/ramenapp/MainActivity.java
@@ -2,24 +2,29 @@ package com.cookandroid.ramenapp;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //타이틀바 텍스트 (공백)
-        ActionBar ab = getSupportActionBar();
-        ab.setTitle("");
+        //위쪽 툴바
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-        //타이틀바 이미지 삽입
-        getSupportActionBar().setIcon(R.drawable.logo_title);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+
+
 }
 
